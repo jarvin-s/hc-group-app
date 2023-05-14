1f679aebbf3dd8d9988071eee8d24eb6ad14deaa
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-15 01:21:59 +0200

    Add icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="sticky top-0 bg-gray-800 shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-3 text-white py-6">
+            <i class="fa-solid fa-code"></i>
             <RouterLink :to="'/'">
                 <div class="flex items-center gap-3 flex-1">
                     <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</p>
