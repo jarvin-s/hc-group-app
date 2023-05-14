fca7778448c5a234e343c33f1b6d5c0062d0dc2d
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-15 01:02:15 +0200

    Fix navigation width

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,14 @@
 <template>
-    <header class="sticky top-0 bg-slate-700 shadow-lg">
-        <nav class="containter flex flex-col sm:flex-row items-center gap-3 text-white py-6">
+    <header class="sticky top-0 bg-gray-800 shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-3 text-white py-6">
+            <RouterLink :to="'/'">
+                <div class="flex items-center gap-3 flex-1">
+                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</p>
+                </div>
+            </RouterLink>
             <RouterLink :to="'/about'">
                 <div class="flex items-center gap-3 flex-1">
-                    <p class="text">Test</p>
+                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</p>
                 </div>
             </RouterLink>
         </nav>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,3 @@
 <template>
-    
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, ullam alias autem illum ipsum minus beatae quo nisi ea iste excepturi perspiciatis et rem praesentium ex, ut repudiandae, sapiente cupiditate.</p>
 </template>
