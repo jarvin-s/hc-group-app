5e38a68403409c9cdd5916b42720a0453e1a9a7a
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-15 18:42:22 +0200

    Add changes (logo, fix nav)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,25 @@
 <template>
     <header class="sticky top-0 bg-gray-800 shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-3 text-white py-6">
-            <i class="fa-solid fa-code"></i>
+            <RouterLink :to="'/'">
+                <img src="public/logo.svg" alt="HC Group">
+            </RouterLink>
             <RouterLink :to="'/'">
                 <div class="flex items-center gap-3 flex-1">
-                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</p>
+                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                        Home</p>
                 </div>
             </RouterLink>
             <RouterLink :to="'/about'">
                 <div class="flex items-center gap-3 flex-1">
-                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</p>
+                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                        About</p>
                 </div>
             </RouterLink>
         </nav>
     </header>
 </template>
 <script setup>
+
 import { RouterView, RouterLink } from 'vue-router'
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,23 @@
 <template>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, ullam alias autem illum ipsum minus beatae quo nisi ea iste excepturi perspiciatis et rem praesentium ex, ut repudiandae, sapiente cupiditate.</p>
+    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, ullam alias autem illum ipsum
+        minus beatae quo nisi ea iste excepturi perspiciatis et rem praesentium ex, ut repudiandae, sapiente cupiditate.</p>
+    <div>
+
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+        <select id="countries"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="US">2015</option>
+            <option value="CA">2016</option>
+            <option value="FR">2017</option>
+            <option value="DE">2018</option>
+            <option value="DE">2019</option>
+            <option value="DE">2020</option>
+            <option value="DE">2021</option>
+        </select>
+
+    </div>
 </template>
+
+<script setup>
+
+</script>
