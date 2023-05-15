c47f7a361e98a1ad5f4fc7ce1d10b075d203ebf4
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-16 00:14:39 +0200

    Update navbar and add dropdowns

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-    <header class="sticky top-0 bg-gray-800 shadow-lg">
-        <nav class="container flex flex-col sm:flex-row items-center gap-3 text-white py-6">
+    <header class="sticky top-0 bg-white shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-3 text-black py-3">
             <RouterLink :to="'/'">
                 <img src="public/logo.svg" alt="HC Group">
             </RouterLink>
             <RouterLink :to="'/'">
                 <div class="flex items-center gap-3 flex-1">
-                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                    <p class="hover:text-sky-600 transition-color duration-200 rounded-md px-3 py-2 text-base font-medium">
                         Home</p>
                 </div>
             </RouterLink>
             <RouterLink :to="'/about'">
                 <div class="flex items-center gap-3 flex-1">
-                    <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                    <p class="hover:text-sky-600 transition-color duration-200 rounded-md px-3 py-2 text-base font-medium">
                         About</p>
                 </div>
             </RouterLink>
@@ -20,6 +20,5 @@
     </header>
 </template>
 <script setup>
-
 import { RouterView, RouterLink } from 'vue-router'
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,39 @@
 <template>
-    <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, ullam alias autem illum ipsum
-        minus beatae quo nisi ea iste excepturi perspiciatis et rem praesentium ex, ut repudiandae, sapiente cupiditate.</p>
-    <div>
-
-        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
-        <select id="countries"
+    <div class="container flex flex-col sm:flex-row items-center gap-3 text-black py-3">
+        <label for="years" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+        <select id="years"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="US">2015</option>
-            <option value="CA">2016</option>
-            <option value="FR">2017</option>
-            <option value="DE">2018</option>
-            <option value="DE">2019</option>
-            <option value="DE">2020</option>
-            <option value="DE">2021</option>
+            <option value="2015">2015</option>
+            <option value="2016">2016</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
         </select>
 
+        <label for="language" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Language</label>
+        <select id="language"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="PHP">PHP</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="Python">Python</option>
+            <option value="C#">C#</option>
+        </select>
+
+        <label for="development" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Development</label>
+        <select id="development"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+            <option value="full">Full stack</option>
+        </select>
+    </div>
+    <div>
+        <myScript />
     </div>
 </template>
 
 <script setup>
-
+import myScript from '@/assets/js/myScript.js';
 </script>
