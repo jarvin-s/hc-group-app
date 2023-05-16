511076c76e012b918369cc74a7665e671cfecaee
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-16 14:04:46 +0200

    Add year filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,114 +1,61 @@
 <template>
-    <div class="container flex flex-col sm:flex-row items-center gap-3 text-black py-3">
-        <label for="years" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
-        <select id="years"
+    <div>
+        <label>Year</label>
+        <select v-model="selectedYear" @change="filterTable"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="2015">2015</option>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-            <option value="2020">2020</option>
-            <option value="2021">2021</option>
-        </select>
-
-        <label for="language" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Language</label>
-        <select id="language"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="PHP">PHP</option>
-            <option value="JavaScript">JavaScript</option>
-            <option value="Python">Python</option>
-            <option value="C#">C#</option>
-        </select>
-
-        <label for="development" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Development</label>
-        <select id="development"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="frontend">Frontend</option>
-            <option value="backend">Backend</option>
-            <option value="full">Full stack</option>
+            <option value="">All</option>
+            <option v-for="year in uniqueYears" :value="year" :key="year">{{ year }}</option>
         </select>
     </div>
-
-
-    <div class="relative overflow-x-auto sm:flex-rowx sm:rounded-lg">
+    <div class="relative overflow-x-auto sm:flex-row">
         <table class="text-sm text-left text-gray-500 sm:table-row dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr class="sm:table-row">
-                    <th scope="col" class="px-6 py-3">
-                        Product name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Color
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Category
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        <span class="sr-only">Edit</span>
-                    </th>
+                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Project</th>
+                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Year</th>
+                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Language</th>
+                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Development</th>
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 sm:table-row hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Apple MacBook Pro 17"
-                    </th>
-                    <td class="px-6 py-4">
-                        Silver
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop
-                    </td>
-                    <td class="px-6 py-4">
-                        $2999
-                    </td>
-                    <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 sm:table-row hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Microsoft Surface Pro
-                    </th>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        Laptop PC
-                    </td>
-                    <td class="px-6 py-4">
-                        $1999
-                    </td>
-                    <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 sm:table-row dark:hover:bg-gray-600">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Magic Mouse 2
-                    </th>
-                    <td class="px-6 py-4">
-                        Black
-                    </td>
-                    <td class="px-6 py-4">
-                        Accessories
-                    </td>
-                    <td class="px-6 py-4">
-                        $99
-                    </td>
-                    <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 sm:table-row dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
+                <tr v-for="row in filteredTableYear" :key="row.id"
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 sm:table-row hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4">{{ row.project }}</td>
+                    <td class="px-6 py-4">{{ row.year }}</td>
+                    <td class="px-6 py-4">{{ row.language }}</td>
+                    <td class="px-6 py-4">{{ row.development }}</td>
                 </tr>
             </tbody>
         </table>
     </div>
 </template>
-
-<script setup>
-// import myScript from '@/assets/js/myScript.js';
+  
+<script>
+export default {
+    data() {
+        return {
+            tableData: [
+                { id: 1, project: 'Sun Tours', year: '2021', language: 'HTML/CSS', development: 'Frontend' },
+                { id: 2, project: 'The Flash', year: '2020', language: 'HTML/CSS', development: 'Frontend' },
+                { id: 3, project: 'Auto Plaza', year: '2015', language: 'HTML/CSS', development: 'Frontend' },
+            ],
+            selectedYear: '',
+        };
+    },
+    computed: {
+        uniqueYears() {
+            const years = new Set();
+            this.tableData.forEach((row) => years.add(row.year));
+            return Array.from(years);
+        },
+        filteredTableYear() {
+            if (this.selectedYear === '') {
+                return this.tableData;
+            } else {
+                return this.tableData.filter((row) => row.year === this.selectedYear);
+            }
+        },
+    },
+};
 </script>
+  
