920fa0c32db8fc7865c29ac7969d2d1e26cc29e6
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-18 21:25:05 +0200

    Add loading/fading animation on each component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,47 @@
 <template>
   <Navbar />
-  <RouterView/>
+  <router-view v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </transition>
+  </router-view>
 </template>
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 </script>
+
+<style>
+.fade-enter-active {
+  animation: coming 0.4s;
+  animation-delay: 0.2s;
+  opacity: 0;
+}
+
+.fade-leave-active {
+  animation: going 0.4s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,39 @@
 <template>
-    <div class="flex flex-col place-content-center sm:flex-row px-4 py-3">
-        <label class="px-2">Year</label>
-        <select v-model="selectedYear" @change="filterTable"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="">All</option>
-            <option v-for="year in uniqueYears" :value="year" :key="year">{{ year }}</option>
-        </select>
-        <label class="px-2">Language</label>
-        <select v-model="selectedLanguage" @change="filterTable"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="">All</option>
-            <option v-for="language in uniqueLanguages" :value="language" :key="language">{{ language }}</option>
-        </select>
-        <label class="px-2">Development</label>
-        <select v-model="selectedDevelopment" @change="filterTable"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="">All</option>
-            <option v-for="development in uniqueDevelopment" :value="development" :key="development">{{ development }}
-            </option>
-        </select>
-    </div>
-    <div class="grid place-items-center p-5 overflow-auto">
-        <table class="overflow-auto flex-row text-sm text-left text-gray-500 sm:table-row dark:text-gray-400">
+    <div class="grid place-items-center p-1 overflow-auto">
+        <div class="flex flex-col place-content-center sm:flex-row px-4 py-3">
+            <label class="px-2">Year</label>
+            <select v-model="selectedYear" @change="filterTable"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="">All</option>
+                <option v-for="year in uniqueYears" :value="year" :key="year">{{ year }}</option>
+            </select>
+            <label class="px-2">Language</label>
+            <select v-model="selectedLanguage" @change="filterTable"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="">All</option>
+                <option v-for="language in uniqueLanguages" :value="language" :key="language">{{ language }}</option>
+            </select>
+            <label class="px-2">Development</label>
+            <select v-model="selectedDevelopment" @change="filterTable"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="">All</option>
+                <option v-for="development in uniqueDevelopment" :value="development" :key="development">{{ development }}
+                </option>
+            </select>
+        </div>
+        <table class="overflow-auto flex-row text-sm text-left sm:table-row">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Project
                     </th>
-                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Year
                     </th>
-                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Language
                     </th>
-                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Development
                     </th>
                 </tr>
