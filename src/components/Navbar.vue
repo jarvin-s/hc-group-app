e8a86b7f68f22a96ab4c8d48dc5194cfc792b690
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-18 16:05:18 +0200

    Change logo from svg to png

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,19 +3,13 @@
         <nav class="container flex flex-col sm:flex-row items-center gap-3 text-black py-3">
             <RouterLink :to="'/'">
                 <div class="flex sm:flex-row">
-                    <img src="/logo.svg" alt="HC Group">
+                    <img src="/logo.png" alt="HC Group">
                 </div>
             </RouterLink>
             <RouterLink :to="'/'">
                 <div class="flex items-center gap-3 flex-1">
                     <p class="hover:text-sky-600 transition-color duration-200 rounded-md px-3 py-2 text-base font-medium">
                         Home</p>
-                </div>
-            </RouterLink>
-            <RouterLink :to="'/about'">
-                <div class="flex items-center gap-3 flex-1">
-                    <p class="hover:text-sky-600 transition-color duration-200 rounded-md px-3 py-2 text-base font-medium">
-                        About</p>
                 </div>
             </RouterLink>
             <div class="flex justify-between items-center hidden sm:block">
